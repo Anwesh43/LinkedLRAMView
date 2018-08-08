caa4d20f382547b2b2ad540bb1a53f81564894a1
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 11:25:07 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrightanglemoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrightanglemoverview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedrightanglemoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.lramview.LRAMView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LRAMView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
