51ffae875ac0f6f8b06c3bd4b24a0b181b76a290
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 10:51:52 +0530

    adding LRAMView class

--- lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
+++ lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
@@ -10,3 +10,21 @@ import android.graphics.Paint
 import android.graphics.Canvas
 
 val nodes : Int = 5
+
+class LRAMView (ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
