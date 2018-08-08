a6b36ec3e0e102a0ecb922dbdedd2e3bd295805b
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 10:30:26 +0530

    importing liraries and defining nodes

--- lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
+++ lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
@@ -3,3 +3,10 @@ package com.anwesh.uiprojects.lramview
 /**
  * Created by anweshmishra on 08/08/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
