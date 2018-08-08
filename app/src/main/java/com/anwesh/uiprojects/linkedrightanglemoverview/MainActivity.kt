860afb9bfe1dac27f345928fa6700223a6d84896
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 11:19:21 +0530

    adding LRAMView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrightanglemoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrightanglemoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrightanglemoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lramview.LRAMView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LRAMView.create(this)
     }
 }
