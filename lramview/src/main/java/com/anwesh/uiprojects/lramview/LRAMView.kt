9f547f965b5797e55b50b23a2cda3d03231821b3
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 10:57:29 +0530

    addign Animator class

--- lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
+++ lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
@@ -47,4 +47,32 @@ class LRAMView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+                    
+                }
+            }
+        }
+    }
 }
