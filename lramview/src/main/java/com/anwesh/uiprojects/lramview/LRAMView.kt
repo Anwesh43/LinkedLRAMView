75687fd5d2b37b2c12c1316f6eedae06c42313e3
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 11:07:33 +0530

    adding LRAMNode class

--- lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
+++ lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
@@ -92,4 +92,49 @@ class LRAMView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRAMNode(var i : Int, val state : State = State()) {
+
+        private var next : LRAMNode? = null
+
+        private var prev : LRAMNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LRAMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRAMNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRAMNode {
+            var curr : LRAMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
