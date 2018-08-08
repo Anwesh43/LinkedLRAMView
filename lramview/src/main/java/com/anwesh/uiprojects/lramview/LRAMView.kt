0f55aa305c3e1005f256d729253d5e5c6f0d8434
Anwesh43 <anweshthecool0@gmail.com>
2018-08-08 11:03:06 +0530

    adding canvas extension method

--- lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
+++ lramview/src/main/java/com/anwesh/uiprojects/lramview/LRAMView.kt
@@ -8,8 +8,25 @@ import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawLRAMNode(i : Int, scale : Float, paint : Paint) {
+    val h : Float = height.toFloat()
+    val w : Float = width.toFloat()
+    val hGap : Float = h / nodes
+    val sc1 : Float = Math.min(0.5f, scale)
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f))
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#4CAF50")
+    save()
+    translate(w/2 - hGap/2, hGap * i + hGap/2)
+    drawLine( hGap * sc1, 0f, hGap, 0f, paint)
+    drawLine(hGap, hGap * sc2, hGap, hGap * sc1, paint)
+    restore()
+}
 
 class LRAMView (ctx : Context) : View(ctx) {
 
@@ -70,7 +87,7 @@ class LRAMView (ctx : Context) : View(ctx) {
                     Thread.sleep(50)
                     view.invalidate()
                 } catch(ex : Exception) {
-                    
+
                 }
             }
         }
